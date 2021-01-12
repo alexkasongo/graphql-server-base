f853d9e3e5ff0b557adc3003894d006a03ea2f44
Alex Kasongo <alex.kasongolenge@yahoo.com>
2021-01-11 23:39:17 -0800

    updated comments

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-// This variable is = true if we are in production mode
+// This variable is = true if we are in production 
 export const __PROD__ = process.env.NODE_ENV === "production";
 export const COOKIE_NAME = "qid";
 export const FORGET_PASSWORD_PREFIX = "forget-password:";
